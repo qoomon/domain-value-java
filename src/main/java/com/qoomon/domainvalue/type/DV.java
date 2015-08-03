7f4acfc2a4885911a27686cf5a6660ddd406dd98
Bengt Brodersen <bengt.brodersen@gmail.com>
2015-08-03 20:14:38 +0200

    feat(cache): DV Constructor Cache

--- src/main/java/com/qoomon/domainvalue/type/DV.java
+++ src/main/java/com/qoomon/domainvalue/type/DV.java
@@ -1,6 +1,8 @@
 package com.qoomon.domainvalue.type;
 
 import java.lang.reflect.Constructor;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 /**
  * A Domain Value is a single not null value wrapper
@@ -17,41 +19,6 @@ public abstract class DV<T> {
 
     protected DV(final T value) {
         this.value = value;
-    }
-
-    /**
-     * @param value  to wrap
-     * @param dvType domain value type
-     * @return true if value is not null, false else
-     */
-    protected static <V, T extends DV<V>> boolean validate(V value, Class<V> valueType, Class<T> dvType) {
-        T dv = newInstance(value, valueType, dvType);
-        return dv.isValid(value);
-    }
-
-    /**
-     * @param value  to wrap
-     * @param dvType domain value type
-     * @return a DV instance
-     */
-    protected static <V, T extends DV<V>> T of(V value, Class<V> valueType, Class<T> dvType) {
-        assert validate(value, valueType, dvType) : value + " is not a valid value for " + dvType.getClass().getSimpleName();
-        return newInstance(value, valueType, dvType);
-    }
-
-    /**
-     * @param value  to wrap
-     * @param dvType domain value type
-     * @return a new DV instance
-     */
-    private static <V, T extends DV<V>> T newInstance(V value, Class<V> valueType, Class<T> dvType) {
-        try {
-            Constructor<T> dvConstructor = dvType.getDeclaredConstructor(valueType);
-            dvConstructor.setAccessible(true);
-            return dvConstructor.newInstance(value);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 
     /**
@@ -95,5 +62,51 @@ public abstract class DV<T> {
     @Override
     public String toString() {
         return value().toString();
+    }
+
+
+    private static Map<Class<? extends DV<?>>, Constructor<? extends DV<?>>> domainValueConstructorCache = new WeakHashMap<>();
+
+
+    /**
+     * @param value  to wrap
+     * @param dvType domain value type
+     * @return true if value is not null, false else
+     */
+    protected static <V, T extends DV<V>> boolean validate(V value, Class<V> valueType, Class<T> dvType) {
+        T dv = newInstance(value, valueType, dvType);
+        return dv.isValid(value);
+    }
+
+    /**
+     * @param value  to wrap
+     * @param dvType domain value type
+     * @return a new specific DV instance
+     */
+    protected static <V, T extends DV<V>> T of(V value, Class<V> valueType, Class<T> dvType) {
+        assert validate(value, valueType, dvType) : value + " is not a valid value for " + dvType.getClass().getSimpleName();
+        return newInstance(value, valueType, dvType);
+    }
+
+    /**
+     * @param value     to wrap
+     * @param valueType value type
+     * @param dvType    domain value type
+     * @return a new specific DV instance
+     */
+
+    private static <V, T extends DV<V>> T newInstance(V value, Class<V> valueType, Class<T> dvType) {
+        try {
+            @SuppressWarnings("unchecked")
+            Constructor<T> dvConstructor = (Constructor<T>) domainValueConstructorCache.get(dvType);
+            if (dvConstructor == null) {
+                dvConstructor = dvType.getDeclaredConstructor(valueType);
+                dvConstructor.setAccessible(true);
+                domainValueConstructorCache.put(dvType, dvConstructor);
+            }
+            return dvConstructor.newInstance(value);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
