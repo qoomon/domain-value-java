32d7e161af1fa72843b996df6d0bf27094a062a7
Bengt Brodersen <bengt.brodersen@gmail.com>
2015-09-22 21:53:54 +0200

    feat: set validateOnConstruction

--- src/main/java/com/qoomon/domainvalue/type/DV.java
+++ src/main/java/com/qoomon/domainvalue/type/DV.java
@@ -1,20 +1,27 @@
 package com.qoomon.domainvalue.type;
 
-import com.qoomon.domainvalue.exception.DVException;
-
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+import com.qoomon.domainvalue.exception.DVException;
+
 /**
  * A Domain Value is a single not null value wrapper
  *
- * @param <T> type of the wrapped value
+ * @param <T>
+ *            type of the wrapped value
  * @author Bengt Brodersen
  */
 public abstract class DV<T> {
 
-    private static Map<Class<? extends DV>, Method> isValidMethodCache = new WeakHashMap<Class<? extends DV>,Method>();
+    private static boolean validateOnConstruction = false;
+
+    static {
+        assert validateOnConstruction = true;
+    }
+
+    private static Map<Class<? extends DV>, Method> isValidMethodCache = new WeakHashMap<Class<? extends DV>, Method>();
 
     /**
      * the wrapped value
@@ -22,20 +29,23 @@ public abstract class DV<T> {
     private final T value;
 
     protected DV(final T value) {
-        assert validate(value) : value + " is NOT a valid value for " + this.getClass().getName();
+        if (validateOnConstruction && !validate(value)) {
+            throw new DVException(value + " is NOT a valid value for " + this.getClass().getName());
+        }
         this.value = value;
     }
 
     private boolean validate(Object value) {
         try {
             Method isValidMethod = isValidMethodCache.get(this.getClass());
-            if(isValidMethod == null){
+            if (isValidMethod == null) {
                 isValidMethod = this.getClass().getMethod("isValid", value.getClass());
                 isValidMethodCache.put(this.getClass(), isValidMethod);
             }
             return (Boolean) isValidMethod.invoke(this.getClass(), value);
         } catch (Exception e) {
-            throw new DVException(this.getClass().getSimpleName() + ".isValid(" + value.getClass().getSimpleName() + ") failed!", e);
+            throw new DVException(
+                    this.getClass().getSimpleName() + ".isValid(" + value.getClass().getSimpleName() + ") failed!", e);
         }
     }
 
@@ -74,10 +84,13 @@ public abstract class DV<T> {
         return value().toString();
     }
 
-
+    public static void validateOnConstruction(boolean validateOnConstruction) {
+        DV.validateOnConstruction = validateOnConstruction;
+    }
 
     /**
-     * @param value to wrap
+     * @param value
+     *            to wrap
      * @return true if value is not null, false else
      */
     protected static boolean isValid(Object value) {
